620d1566760dd1015dd0a016459e08222066de85
Manoprasanth MATHAVAN <manoprasanth.mathavan@gmail.com>
2024-12-05 17:04:10 +0530

    add day-5 part-1

---
+++ day-5/part-1/main.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const [order, arr] = fs.readFileSync("./input.txt", "utf-8").split("\n\n");
+const orderArr = order.split("\n");
+mainArr = [];
+arr.split("\n").map((e) => {
+  mainArr.push(e.split(","));
+});
+
+resultArr = [];
+mainArr.forEach((element) => {
+  state = true;
+  for (let i = 0; i < element.length - 1; i++) {
+    for (let j = i + 1; j < element.length; j++) {
+      const searchStr = `${element[i]}|${element[j]}`;
+      if (order.includes(searchStr) == false) {
+        state = false;
+        break;
+      }
+    }
+    if (state === false) break;
+  }
+  if (state === true) resultArr.push(element);
+});
+
+let result = 0;
+resultArr.forEach((element) => {
+  result += Number(element[Math.floor(element.length / 2)]);
+});
+
+console.log(result);
